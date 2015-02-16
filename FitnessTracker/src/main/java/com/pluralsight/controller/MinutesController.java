321ec3ad1ee524b3f806612826e4246ce23b8da1
leandrocombinato <leandro.combinato@quantumit.com.br>
2015-02-16 17:39:25 -0200

    Chaining

--- FitnessTracker/src/main/java/com/pluralsight/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/pluralsight/controller/MinutesController.java
@@ -13,7 +13,12 @@ public class MinutesController {
 	@RequestMapping(value = "addMinutes")
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise){
 		System.out.println("exercise: "+ exercise.getMinutes());
-		return "addMinutes";
+		return "redirect:addMoreMinutes.html";
 	}
 
+	@RequestMapping(value = "addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise){
+		System.out.println("exercising: "+ exercise.getMinutes());
+		return "addMinutes";
+	}
 }
